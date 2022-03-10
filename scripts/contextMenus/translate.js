d83604143f451c28947bec5da59d8b267940a201
WolfGames <58263600+WolfGames@users.noreply.github.com>
2022-03-10 21:32:26 +0100

    fixed language country codes in translation feature

--- scripts/contextMenus/translate.js
+++ scripts/contextMenus/translate.js
@@ -1,14 +1,20 @@
 const translate = require('@vitalets/google-translate-api');
 
+var langReplacements = {
+    "en":"us",
+    "ja":"jp",
+    "zh-CN":"cn",
+    "zh-TW":"tw"
+}
+
 console.log("target message "+interaction.targetMessage.content)
 translate(interaction.targetMessage.content).then(res => {
+    var country_code = res.from.language.iso.toLowerCase()
+    if(country_code in langReplacements){country_code = country_code.replace(country_code,langReplacements[country_code])}
+
     interaction.reply({embeds:[{
-        "title":"Translating from :flag_"+res.from.language.iso.toLowerCase().replace("zh-","")+":",
-        "description":"[Jump to original message](https://discord.com/channels/"+interaction.guild.id+"/"+interaction.channel.id+"/"+interaction.targetMessage.id+")",
-        "fields":[{
-            "name":"result",
-            "value":res.text
-        }],
+        "title":"Translating from :flag_"+country_code+":",
+        "description":"[Jump to original message](https://discord.com/channels/"+interaction.guild.id+"/"+interaction.channel.id+"/"+interaction.targetMessage.id+")\n"+res.text,
         "color":"#3399FF"
     }]})
 
